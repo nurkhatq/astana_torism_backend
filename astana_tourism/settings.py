24fd45e74ad2768b7ea9e5ca563111a26f7fd105
nurkhatq <dnurhat140@gmail.com>
2024-12-05 16:00:16 +0500

    final2

--- astana_tourism/settings.py
+++ astana_tourism/settings.py
@@ -121,7 +121,6 @@ TEMPLATES = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 GOOGLE_PLACES_API_KEY = 'AIzaSyBHQ_sAOG7Nndu5soo5lAp6KDDRZIv4EBg'
 
 
